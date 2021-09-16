db11e8966e6f66118418d83f268d97e8db266049
Jamesb255225 <88412089+Jamesb255225@users.noreply.github.com>
2021-09-16 14:39:27 +0930

    player.cpp first iteration
    second commit

--- player.h
+++ player.h
@@ -7,7 +7,7 @@ using namespace std;
 class player
 {
 		public:
-			string player_position;
+			//string player_position;
 
 			int tackle_stat;
 			int mark_stat;
@@ -20,7 +20,7 @@ class player
 			int stamina_stat;
 
 			int get_attk_stat(int mark_stat, int goal_kick_stat, int handball_stat, int kick_stat, int speed_stat, int stamina_stat);
-			int get_def_stat(int tackle_stat, int mark_stat, int def_awareness_stat, int spoil_stat, int speed_stat, int stamina_stat);
-			int get_player_overall(int get_attk_stat, int get_def_stat);
-}
+			//int get_def_stat(int tackle_stat, int mark_stat, int def_awareness_stat, int spoil_stat, int speed_stat, int stamina_stat);
+			//int get_player_overall(int get_attk_stat, int get_def_stat);
+};
 #endif
