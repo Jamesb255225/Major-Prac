9edcbcb73176d434b57e3f4bddcbfab07e7dee58
Jamesb255225 <88412089+Jamesb255225@users.noreply.github.com>
2021-10-06 11:51:19 +1030

    delete old work

--- player.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-#include <"player.h">
-using namespace std;
-
-int get_attk_stat(int mark_stat, int goal_kick_stat, int handball_stat, int kick_stat, int speed_stat, int stamina_stat)
-{
-	return ((mark_stat+goal_kick_stat+handball_stat+kick_stat+speed_stat+stamina_stat)/6)
-}
